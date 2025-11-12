0267bfc988cac89bff0d8c50854162c5cd11f22e
DanielPot217 <danielp0517@gmail.com>
2025-11-11 23:22:23 -0500

    Implemented Validation

--- src/routes/genreRoutes.js
+++ src/routes/genreRoutes.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import * as genreController from '../controllers/genreController.js';
-import { validateGenreQuery, validateGenreId, validateGenre } from '../middleware/genreValidators.js'
+import { validateGenreQuery, validateGenreId, validateGenre } from '../middleware/genreValidators.js';
+
+import authenticate from '../middleware/authenticate.js';
+import { authorizeRole } from '../middleware/authorizeRole.js';
 
 // To Do:
-//      Authorization
-//      Documentation
+//      Fix Error When Deleting Genre That has books attached(Validation)
 
 const router = express.Router();
 
-router.get('/', validateGenreQuery, genreController.getGenresHandler);
-router.get('/:id', validateGenreId, genreController.getGenreByIdHandler);
-router.get('/:id/books', validateGenreId, validateGenreQuery, genreController.getGenreBooksByIdHandler);
+router.get('/', authenticate, authorizeRole('MANAGER', 'LIBRARIAN'), validateGenreQuery, genreController.getGenresHandler);
+router.get('/:id', authenticate, authorizeRole('MANAGER', 'LIBRARIAN'), validateGenreId, genreController.getGenreByIdHandler);
+router.get('/:id/books', authenticate, authorizeRole('MANAGER', 'LIBRARIAN'), validateGenreId, validateGenreQuery, genreController.getGenreBooksByIdHandler);
 
-router.post('/', validateGenre, genreController.createGenreHandler);
+router.post('/', authenticate, authorizeRole('MANAGER'), validateGenre, genreController.createGenreHandler);
 
-router.put('/:id', validateGenreId, validateGenre, genreController.updateGenreHandler);
+router.put('/:id', validateGenreId, authenticate, authorizeRole('MANAGER'), validateGenre, genreController.updateGenreHandler);
 
-router.delete('/:id', validateGenreId, genreController.deleteGenreHandler);
+router.delete('/:id', validateGenreId, authenticate, authorizeRole('MANAGER'), genreController.deleteGenreHandler);
 
 export default router;
